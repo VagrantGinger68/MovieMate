c0c195189dd3adf9898a662a5d4a63c85a18468a
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-10-28 17:21:04 -0400

    Update the HomoRoute to contain the categories

--- client/src/routes/HomeRoute.tsx
+++ client/src/routes/HomeRoute.tsx
@@ -1,12 +1,19 @@
-import MovieList from "../components/MovieList";
-import GenreList from "../components/GenreList";
-
+import NowPlayingList from "../components/Categories/NowPlayingList";
+import PopularList from "../components/Categories/PopularList";
+import TopRatedList from "../components/Categories/TopRatedList";
+import UpcomingList from "../components/Categories/UpcomingList";
 
 const HomeRoute = () => {
   return (
     <>
-      <GenreList />
-      <MovieList />
+      <h1>Popular</h1>
+      <PopularList />
+      <h1>Upcoming</h1>
+      <UpcomingList />
+      <h1>Now Playing</h1>
+      <NowPlayingList />
+      <h1>Top Rated</h1>
+      <TopRatedList />
     </>
   )
 }

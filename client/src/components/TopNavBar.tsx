36809ff099fbcac1f45825f7971e88386bc6962f
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-28 11:06:54 -0400

    created components for front-end

---
+++ client/src/components/TopNavBar.tsx
@@ -0,0 +1,7 @@
+const TopNavBar = () => {
+  return (
+    <h1>Top Nav Bar</h1>
+  )
+}
+
+export default TopNavBar;
---
+++ client/src/routes/HomeRoute.tsx
@@ -0,0 +1,14 @@
+import TopNavBar from "../components/TopNavBar";
+import Footer from "../components/Footer";
+
+
+const HomeRoute = () => {
+  return (
+    <>
+      <TopNavBar />
+      <Footer />
+    </>
+    )
+}
+
+export default HomeRoute;

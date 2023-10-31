17dd45bfd3b15807b4f555d8a677443b2a40db7c
RomelDonovan <romeldonovan@live.ca>
2023-10-31 16:19:35 -0400

    Add react router to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import TopNavBar from './components/TopNavBar';
 import HomeRoute from './routes/HomeRoute';
 import MovieRoute from './routes/MovieRoute';
 import CategoryRoute from './routes/CategoryRoute';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 
 const App = () => {
 
@@ -28,15 +29,20 @@ const App = () => {
 
   return (
     <div>
-      <TopNavBar changeCategoryName={changeCategoryName} changeGenre={changeGenre} />
+      {/* <TopNavBar changeCategoryName={changeCategoryName} changeGenre={changeGenre} />
       {/* {!movieId &&
         <HomeRoute changeMovieId={changeMovieId} />
       }
       {movieId &&
         <MovieRoute id={movieId} changeMovieId={changeMovieId} />
       } */}
-      <CategoryRoute changeMovieId={changeMovieId} categoryName={categoryName} changeGenre={changeGenre} genre={genre}/>
-      <Footer />
+      {/* <CategoryRoute changeMovieId={changeMovieId} categoryName={categoryName} changeGenre={changeGenre} genre={genre}/>
+      <Footer /> */}
+      <Router>
+      <Routes>
+            <Route path="/" element={<HomeRoute changeMovieId={changeMovieId}/>} />
+      </Routes>
+      </Router>
     </div>
   )
 }

2a8ce7a0021d3e547a55e0319f64515583ff7acb
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-10-31 18:18:04 -0400

    add year and rating to movielist

--- client/src/components/MovieList.tsx
+++ client/src/components/MovieList.tsx
@@ -3,7 +3,9 @@ import MovieListItem from "./MovieListItem";
 interface Movies {
   title: string,
   id: number,
-  poster_path: string
+  poster_path: string,
+  release_date: string,
+  vote_average: number
 }
 interface CategoryProps {
   movies: Movies[]
@@ -12,13 +14,13 @@ interface CategoryProps {
 
 const MovieList: React.FC<CategoryProps> = ({ movies, changeMovieId }) => {
   return (
-    <>
-      {movies.map(({ title, id, poster_path }) => {
+    <div className="flex overflow-x-auto space-x-10 py-5">
+      {movies.map(({ title, id, poster_path, release_date, vote_average }) => {
         return (
-          <MovieListItem key={id} title={title} poster_path={poster_path} changeMovieId={changeMovieId} id={id} />
+          <MovieListItem key={id} title={title} poster_path={poster_path} changeMovieId={changeMovieId} id={id} release_date={release_date} vote_average={vote_average}/>
         )
       })}
-    </>
+    </div>
   )
 }
 

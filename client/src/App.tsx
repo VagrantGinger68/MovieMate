0881b3140971135712cee8f1a40b7f606f8eca42
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-10-30 20:16:01 -0400

    Add state to track movie id

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Footer from './components/Footer';
 import TopNavBar from './components/TopNavBar';
@@ -6,11 +7,18 @@ import MovieRoute from './routes/MovieRoute';
 
 const App = () => {
 
+  const [movieId, setMovieId] = useState(238);
+
+  const changeMovieId = (newId: number) => {
+    setMovieId(newId);
+  };
+
   return (
     <div>
+      <button onClick={() => changeMovieId(278)}>Change Movie</button>
       <TopNavBar />
       {/* <HomeRoute /> */}
-      <MovieRoute />
+      <MovieRoute id={movieId} />
       <Footer />
     </div>
   )

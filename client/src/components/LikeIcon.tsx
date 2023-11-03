add68efe1db89dde04320b942e337cada9a6fde4
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-11-03 18:11:57 -0400

    store the like status in localstorage

--- client/src/components/LikeIcon.tsx
+++ client/src/components/LikeIcon.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 interface MovieIdProp {
   movieId: number;
@@ -13,41 +13,44 @@ const LikeIcon: React.FC<MovieIdProp> = ({ movieId }) => {
       user_id: userId,
       movie_id: movieId,
     };
-    setLike(!like)
-    if (!like === true) {
 
-      fetch('http://localhost:3000/liked_movies', {
-        method: 'POST',
+    // Update the like state and store it in local storage
+    setLike(!like);
+    localStorage.setItem(`like-${movieId}`, !like ? "true" : "false");
+
+    if (!like) {
+      fetch("http://localhost:3000/liked_movies", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       })
         .then((response) => {
           if (response.status === 201) {
-            console.log('Like created successfully');
+            console.log("Like created successfully");
           } else {
-            console.error('Failed to create a like');
+            console.error("Failed to create a like");
           }
         })
         .catch((error) => {
-          console.error('Request failed:', error);
+          console.error("Request failed:", error);
         });
     } else {
       const urlLiked = `http://localhost:3000/liked_movies/find_by_movie_and_user/${movieId}/${1}`;
       const optionsLiked = {
-        method: 'GET',
+        method: "GET",
         headers: {
-          'Content-Type': 'application/json',
-        }
+          "Content-Type": "application/json",
+        },
       };
 
       fetch(urlLiked, optionsLiked)
-        .then(res => res.json())
-        .then(json => setLikedMoviesId(json))
-        .catch(err => console.error('error:' + err));
+        .then((res) => res.json())
+        .then((json) => setLikedMoviesId(json))
+        .catch((err) => console.error("error:" + err));
     }
-  }
+  };
 
   useEffect(() => {
     if (likedMoviesId.id) {
@@ -62,27 +65,36 @@ const LikeIcon: React.FC<MovieIdProp> = ({ movieId }) => {
     }
   }, [likedMoviesId.id]);
 
+  useEffect(() => {
+    // Retrieve the like state from local storage when the component mounts
+    const storedLike = localStorage.getItem(`like-${movieId}`);
+    if (storedLike === "true" || storedLike === "false") {
+      setLike(storedLike === "true");
+    }
+  }, [movieId]);
+
   const handleMovie = (movieId) => {
     fetch("http://localhost:3000/movies", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ movieId: movieId })
+      body: JSON.stringify({ movieId: movieId }),
     });
-  }
+  };
 
   const handleClick = (movieId, userId) => {
     handleMovie(movieId);
     toggleLike(movieId, userId);
-  }
+  };
 
   return (
     <button
       type="button"
       className={`
       hover:text-blue-700 border border-blue-700 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center
-      ${like && 'bg-blue-700 text-white dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500'}
+      ${like &&
+        "bg-blue-700 text-white dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500"}
     `}
       onClick={() => handleClick(movieId, 1)}
     >
@@ -95,9 +107,8 @@ const LikeIcon: React.FC<MovieIdProp> = ({ movieId }) => {
       >
         <path d="M3 7H1a1 1 0 0 0-1 1v8a2 2 0 0 0 4 0V8a1 1 0 0 0-1-1Zm12.954 0H12l1.558-4.5a1.778 1.778 0 0 0-3.331-1.06A24.859 24.859 0 0 1 6 6.8v9.586h.114C8.223 16.969 11.015 18 13.6 18c1.4 0 1.592-.526 1.88-1.317l2.354-7A2 2 0 0 0 15.954 7Z" />
       </svg>
-
     </button>
-  )
-}
+  );
+};
 
-export default LikeIcon
+export default LikeIcon;

2d98a35c818f274df35cb4887f76bad97816fee3
RomelDonovan <romeldonovan@live.ca>
2023-11-03 22:52:52 -0400

    Clean up

--- client/src/components/Categories/CategoryMovieList.tsx
+++ client/src/components/Categories/CategoryMovieList.tsx
@@ -1,28 +1,32 @@
-import CategoryMovieListItem from "./CategoryMovieListItem";
+import CategoryMovieListItem from './CategoryMovieListItem';
 
 interface CategoryProps {
-  movies: Movies[]
+  movies: Movies[];
 }
+
 interface Movies {
-  title: string,
-  id: number,
-  poster_path: string,
-  release_date: string,
-  vote_average: number
+  title: string;
+  id: number;
+  poster_path: string;
+  release_date: string;
+  vote_average: number;
 }
 
 const CategoryMovieList: React.FC<CategoryProps> = ({ movies }) => {
   return (
     <div className="flex flex-row justify-evenly flex-wrap">
-      {
-        movies.map(({ title, id, poster_path, release_date, vote_average }) => {
-          return (
-            <CategoryMovieListItem key={id} title={title} poster_path={poster_path} id={id} release_date={release_date} vote_average={vote_average} />
-          )
-        })
-      }
+      {movies.map(({ title, id, poster_path, release_date, vote_average }) => (
+        <CategoryMovieListItem
+          key={id}
+          title={title}
+          poster_path={poster_path}
+          id={id}
+          release_date={release_date}
+          vote_average={vote_average}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default CategoryMovieList;

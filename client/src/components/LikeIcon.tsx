4f26fcb65f3aa5ac30b68d0f68cd753e23472417
RomelDonovan <romeldonovan@live.ca>
2023-11-03 22:36:31 -0400

    Change like icon to star icon

--- client/src/components/LikeIcon.tsx
+++ client/src/components/LikeIcon.tsx
@@ -7,6 +7,7 @@ interface MovieIdProp {
 const LikeIcon: React.FC<MovieIdProp> = ({ movieId }) => {
   const [like, setLike] = useState(false);
   const [likedMoviesId, setLikedMoviesId] = useState({});
+  const [currentColor, setCurrentColor] = useState('#FFD700');
 
   const toggleLike = (movieId, userId) => {
     const data = {
@@ -88,26 +89,44 @@ const LikeIcon: React.FC<MovieIdProp> = ({ movieId }) => {
     toggleLike(movieId, userId);
   };
 
+  const changeColor = () => {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    setCurrentColor(randomColor);
+  };
+
+  useEffect(() => {
+    if (like) {
+      const colorInterval = setInterval(changeColor, 100); // Change color every 1 second
+
+      return () => {
+        clearInterval(colorInterval); // Clear the interval when `like` becomes false
+      };
+    }
+  }, [like]);
+
   return (
-    <button
-      type="button"
-      className={`
-      hover:text-blue-700 border border-blue-700 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center
-      ${like &&
-        "bg-blue-700 text-white dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500"}
-    `}
-      onClick={() => handleClick(movieId, 1)}
-    >
-      <svg
-        className="w-4 h-4"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        viewBox="0 0 18 18"
-      >
-        <path d="M3 7H1a1 1 0 0 0-1 1v8a2 2 0 0 0 4 0V8a1 1 0 0 0-1-1Zm12.954 0H12l1.558-4.5a1.778 1.778 0 0 0-3.331-1.06A24.859 24.859 0 0 1 6 6.8v9.586h.114C8.223 16.969 11.015 18 13.6 18c1.4 0 1.592-.526 1.88-1.317l2.354-7A2 2 0 0 0 15.954 7Z" />
-      </svg>
-    </button>
+    <>
+      {(like === true && movieId === 502356) ? (<button
+        type="button"
+        onClick={() => handleClick(movieId, 1)}>
+        <svg className="w-6 h-6 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill={currentColor} viewBox="0 0 22 20">
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+      </button>) : ""}
+      {(like === true && movieId !== 502356) ? (<button
+        type="button"
+        onClick={() => handleClick(movieId, 1)}>
+          <svg className="w-6 h-6 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+        </button>) : (like === false &&
+      <button type="button" onClick={() => handleClick(movieId, 1)}>
+        <svg className="w-6 h-6 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+          <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+      </button>
+      )}
+    </>
   );
 };
 

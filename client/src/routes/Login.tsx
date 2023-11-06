109e4c5a9b792a78f50a55afcb977dd3bfe51513
RomelDonovan <romeldonovan@live.ca>
2023-11-06 11:16:06 -0500

    Change genre and login background

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -42,7 +42,7 @@ const Login: React.FC<CookieProp> = ({ changeCookie }) => {
   }
 
   return (
-    <form className="mx-auto flex min-h-screen w-full items-center justify-center bg-gray-900 text-white">
+    <form className="mx-auto flex min-h-screen w-full items-center justify-center bg-[#282828] text-white">
       <section className="flex w-[30rem] flex-col space-y-10">
         <div className="text-center text-4xl font-medium">Log In</div>
         <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
@@ -61,7 +61,7 @@ const Login: React.FC<CookieProp> = ({ changeCookie }) => {
             onChange={(e) => (setPassword(e.target.value))}
           />
         </div>
-        <button onClick={handleClick} className="transform rounded-sm bg-indigo-600 py-2 font-bold duration-300 hover:bg-indigo-400">
+        <button onClick={handleClick} className="transform rounded-sm bg-[#4477CE] py-2 font-bold duration-300 hover:bg-indigo-400">
           LOG IN
         </button>
       </section>

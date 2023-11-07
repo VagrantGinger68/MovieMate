340fc0cac709d96d0b2507e61a54fba1f38cddf6
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-11-07 11:02:21 -0500

    add seeds for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,12 @@
 #   Character.create(name: "Luke", movie: movies.first)
 User.create(name: "Bob Doe", email: "bob@bob.com", password: "123")
 User.create(name: "Jane Smith", email: "jane@jane.com", password: "123")
+User.create(name: "Labber", email: "labber@labber.com", password: "password")
 
 Movie.create(movieId: 507089)
+Movie.create(movieId: 502356)
+
+Message.create(content:"Peaches was 🔥", movieId: 502356, username: "MarioLover35", created_at: "2023-11-06 18:30:25")
+Message.create(content:"BRUH 😭 🪠", movieId: 502356, username: "BowserLover", created_at: "2023-11-06 18:34:25")
 
 # LikedMovie.create(user_id:1, movie_id: 507089)
